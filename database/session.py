5293d6882b494732c0891722ab0ea2d9ca7a3a61
Slava <yvi86248624@gmail.com>
2025-08-07 19:06:32 +0300

    добавление asyncpg в poetry

--- database/session.py
+++ database/session.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-from config import settings
+from core.config import settings
 
 engine = create_async_engine(settings.sql_db_url, echo=True)
 
